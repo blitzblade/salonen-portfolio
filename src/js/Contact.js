7e8190f5a7f7c86b8cabb0eb9e7b34d9626878a7
Santeri Salonen <santeri.salonen@foodie.fm>
2018-07-04 02:04:24 +0300

    make contact form work

--- src/js/Contact.js
+++ src/js/Contact.js
@@ -1,51 +1,86 @@
 import React, { Component } from 'react';
-import { FormGroup, FormControl, ControlLabel } from 'react-bootstrap'
+import axios from 'axios';
+import { FormGroup, FormControl, Button } from 'react-bootstrap';
 import '../css/Contact.css';
 
 class Contact extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            sent: false,
-            nameValue: '',
-            emailValue: '',
-            msgValue: ''
-        }
+    successMessage: '';
+    state = {
+        sent: false,
+        nameValue: '',
+        emailValue: '',
+        msgValue: '',
+        data: {},
+        successMessageChanged: false
     }
     handleChange(e) {
         this.setState({[e.target.name]: e.target.value});
-        console.log(this.state.nameValue);
+    }
+    handleSubmit(e) {
+        e.preventDefault();
+        const self = this;
+        let params = new URLSearchParams();
+        params.append('app', 'salonen-portfolio');
+        params.append('name', this.state.nameValue);
+        params.append('email', this.state.emailValue);
+        params.append('message', this.state.msgValue);
+        axios.post('http://kriisiryhma.com/data/sendmail.php', params)  
+          .then(function (response) {
+            self.setState({sent: true});
+            console.log(response);
+            self.setState({data: response.data});
+            console.log(self.state);
+            self.changeMessage();
+          })
+          .catch(function (error) {
+            console.log(error);
+          });  
+    }
+    changeMessage() {
+        if (this.state.sent) {
+            if (this.state.data.status === "ok") {
+                this.successMessage = <div className="success"><strong>Thank you! Your message has been sent</strong></div>;
+            } else {
+                this.successMessage = <div className="error"><strong>Error: {this.state.data.message}</strong></div>;
+            }
+            // Must set state to trigger componentDidUpdate and to re-render
+            this.setState({successMessageChanged: true})
+        }
     }
     render() {
         return (
             <div className="contact">
                 <h2 className="subhead">Contact</h2>
-                <form>
-                    <FormGroup controlId="contactForm">
-                        <ControlLabel>Name:</ControlLabel>
+                <div id="message">
+                    {this.successMessage}
+                </div>
+                <form id="contactform" onSubmit={this.handleSubmit.bind(this)} method="post">
+                    <FormGroup controlId="contactform">
+                        <h3>Contact Me</h3>
                         <FormControl
-                        onChange={this.handleChange.bind(this)}
-                        value={this.state.nameValue}
-                        type="text"
-                        name="nameValue"
-                        placeholder="Name"
+                            onChange={this.handleChange.bind(this)}
+                            value={this.state.nameValue}
+                            type="text"
+                            name="nameValue"
+                            placeholder="Name"
                         />
-                        <ControlLabel>Email:</ControlLabel>
+                        <br />
                         <FormControl
-                        onChange={this.handleChange.bind(this)}
-                        value={this.state.emailValue}
-                        name="emailValue"
-                        type="email"
-                        placeholder="Name"
+                            onChange={this.handleChange.bind(this)}
+                            value={this.state.emailValue}
+                            name="emailValue"
+                            type="email"
+                            placeholder="Email"
                         />
-                        <ControlLabel>Message:</ControlLabel>
+                        <h3>Your message here</h3>
                         <FormControl
-                        onChange={this.handleChange.bind(this)}
-                        value={this.state.msgValue}
-                        name="msgValue"
-                        componentClass="textarea"
-                        placeholder="Write your message here"
+                            onChange={this.handleChange.bind(this)}
+                            value={this.state.msgValue}
+                            name="msgValue"
+                            componentClass="textarea"
+                            placeholder="Write your message here"
                         />
+                        <Button id="submit-btn" type="submit">Submit</Button>
                     </FormGroup>
                 </form>
             </div>
